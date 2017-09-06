75de68361f8b03f9821bcb92c20550ea29e3bd0e
wangtao <niitwangtao@163.com>
2017-09-06 15:23:41 +0800

    ser-cli OK

--- SocketClie/client/client/client.cpp
+++ SocketClie/client/client/client.cpp
@@ -23,46 +23,119 @@
 
 
 
-/*
-  client.cpp boost socket 客户端
+///*
+//  client.cpp boost socket 客户端
+//
+//	通过tcp::socket类定义一个tcp client对象socket
+//	通过connect函数连接服务器，打开socket连接。
+//	通过read_some函数来读数据
+//*/
+//#include "stdafx.h"
+//#include "cstdio"
+//#include "iostream"
+//#include "string"
+//#include "boost/array.hpp"
+//#include "boost/asio.hpp"
+//
+//using boost::asio::ip::tcp;
+//
+//int main(int argc, char* argv[]) {
+//	try {
+//		boost::asio::io_service io_service;
+//		tcp::endpoint end_point(boost::asio::ip::address::from_string("127.0.0.1"), 3200);
+//
+//		tcp::socket socket(io_service);
+//		socket.connect(end_point);
+//
+//		for (;;) {
+//			boost::array<char, 128> buf;
+//			boost::system::error_code error;
+//
+//			size_t len = socket.read_some(boost::asio::buffer(buf), error);
+//
+//			if (error == boost::asio::error::eof)
+//				break; // Connection closed cleanly by peer.
+//			else if (error)
+//				throw boost::system::system_error(error); // Some other error.
+//
+//			std::cout.write(buf.data(), len);
+//		}
+//	} catch (std::exception& e) {
+//		std::cerr << e.what() << std::endl;
+//	}
+//
+//	return 0;
+//}
 
-	通过tcp::socket类定义一个tcp client对象socket
-	通过connect函数连接服务器，打开socket连接。
-	通过read_some函数来读数据
-*/
+
 #include "stdafx.h"
-#include "cstdio"
 #include "iostream"
-#include "string"
-#include "boost/array.hpp"
+#include "boost/shared_ptr.hpp"
 #include "boost/asio.hpp"
+#include "boost/asio/placeholders.hpp"
+#include "boost/system/error_code.hpp"
+#include "boost/bind/bind.hpp"
 
-using boost::asio::ip::tcp;
+using namespace std;
+using namespace boost::asio;
 
-int main(int argc, char* argv[]) {
-	try {
-		boost::asio::io_service io_service;
-		tcp::endpoint end_point(boost::asio::ip::address::from_string("127.0.0.1"), 3200);
+class client {
+	typedef client this_type;
+	typedef ip::tcp::acceptor acceptor_type;
+	typedef ip::tcp::endpoint endpoint_type;
+	typedef ip::tcp::socket socket_type;
+	typedef ip::address address_type;
+	typedef boost::shared_ptr<socket_type> sock_ptr;
+	typedef vector<char> buffer_type;
 
-		tcp::socket socket(io_service);
-		socket.connect(end_point);
-
-		for (;;) {
-			boost::array<char, 128> buf;
-			boost::system::error_code error;
-
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-			if (error == boost::asio::error::eof)
-				break; // Connection closed cleanly by peer.
-			else if (error)
-				throw boost::system::system_error(error); // Some other error.
-
-			std::cout.write(buf.data(), len);
-		}
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
+private:
+	io_service m_io;
+	buffer_type m_buf;
+	endpoint_type m_ep;
+public:
+	client() : m_buf(100, 0), m_ep(address_type::from_string("127.0.0.1"), 6688) {
+		start();
 	}
 
+	void run() {
+		m_io.run();
+	}
+
+	void start() {
+		sock_ptr sock(new socket_type(m_io));
+		sock->async_connect(m_ep, boost::bind(&this_type::conn_handler, this, boost::asio::placeholders::error, sock));
+	}
+
+	void conn_handler(const boost::system::error_code&ec, sock_ptr sock) {
+		if (ec) {
+			return;
+		}
+		cout << "Receive from " << sock->remote_endpoint().address() << ": " << endl;
+		sock->async_read_some(buffer(m_buf), boost::bind(&client::read_handler, this, boost::asio::placeholders::error, sock));
+	}
+
+	void read_handler(const boost::system::error_code&ec, sock_ptr sock) {
+		if (ec) {
+			return;
+		}
+		sock->async_read_some(buffer(m_buf), boost::bind(&client::read_handler, this, boost::asio::placeholders::error, sock));
+		cout << &m_buf[0] << endl;
+	}
+};
+
+int main() {
+	try {
+		cout << "Client start." << endl;
+		//client cl;
+		//cl.run();
+		for (int a = 10; a < 20; a = a + 1) {
+			cout << "a 的值：" << a << endl;
+			client cl;
+			cl.run();
+		}
+	} catch (std::exception &e) {
+		cout << e.what() << endl;
+	}
+	getchar();
 	return 0;
 }
--- SocketSer/server/server/server.cpp
+++ SocketSer/server/server/server.cpp
@@ -22,39 +22,103 @@
 //}
 
 
-/*
-  server.cpp Boost socket server 控制台入口
+///*
+//  server.cpp Boost socket server 控制台入口
+//
+//	通过tcp::acceptor类创建一个tcp server对象，并绑定端口（也可以不在构造器中自动绑定，而通过bind函数手动绑定）
+//	通过accept函数获取远端连接
+//	通过远端连接的write_some函数将数据发往客户端
+//*/
+//#include "stdafx.h"
+//#include "ctime"
+//#include "iostream"
+//#include "string"
+//#include "boost\asio.hpp"
+//
+//using namespace boost;
+//using boost::asio::ip::tcp;
+//
+//int main() {
+//	try {
+//		asio::io_service io_service;
+//		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 3200));
+//
+//		for (;;) {
+//			tcp::socket socket(io_service);
+//			acceptor.accept(socket);
+//
+//			time_t now = time(0);
+//			std::string message = ctime(&now);
+//
+//			system::error_code ignored_error;
+//			socket.write_some(asio::buffer(message), ignored_error);
+//		}
+//	} catch (std::exception& e) {
+//		std::cerr << e.what() << std::endl;
+//	}
+//
+//	return 0;
+//}
 
-	通过tcp::acceptor类创建一个tcp server对象，并绑定端口（也可以不在构造器中自动绑定，而通过bind函数手动绑定）
-	通过accept函数获取远端连接
-	通过远端连接的write_some函数将数据发往客户端
-*/
 #include "stdafx.h"
-#include "ctime"
 #include "iostream"
-#include "string"
-#include "boost\asio.hpp"
+#include "boost/shared_ptr.hpp"
+#include "boost/asio.hpp"
+#include "boost/asio/placeholders.hpp"
+#include "boost/system/error_code.hpp"
+#include "boost/bind/bind.hpp"
 
-using namespace boost;
-using boost::asio::ip::tcp;
+using namespace std;
+using namespace boost::asio;
+
+class server {
+	typedef server this_type;
+	typedef ip::tcp::acceptor acceptor_type;
+	typedef ip::tcp::endpoint endpoint_type;
+	typedef ip::tcp::socket socket_type;
+	typedef ip::address address_type;
+	typedef boost::shared_ptr<socket_type> sock_ptr;
+
+private:
+	io_service m_io;
+	acceptor_type m_acceptor;
+
+public:
+	server() : m_acceptor(m_io, endpoint_type(ip::tcp::v4(), 6688)) {
+		accept();
+	}
+
+	void run() { m_io.run(); }
+
+	void accept() {
+		sock_ptr sock(new socket_type(m_io));
+		m_acceptor.async_accept(*sock, boost::bind(&this_type::accept_handler, this, boost::asio::placeholders::error, sock));
+	}
+
+	void accept_handler(const boost::system::error_code& ec, sock_ptr sock) {
+		if (ec) {
+			return;
+		}
+
+		cout << "Client:";
+		cout << sock->remote_endpoint().address() << endl;
+		sock->async_write_some(buffer("hello asio"), boost::bind(&this_type::write_handler, this, boost::asio::placeholders::error));
+		// 发送完毕后继续监听，否则io_service将认为没有事件处理而结束运行
+		accept();
+	}
+
+	void write_handler(const boost::system::error_code&ec) {
+		cout << "send msg complete" << endl;
+	}
+};
 
 int main() {
 	try {
-		asio::io_service io_service;
-		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 3200));
-
-		for (;;) {
-			tcp::socket socket(io_service);
-			acceptor.accept(socket);
-
-			time_t now = time(0);
-			std::string message = ctime(&now);
-
-			system::error_code ignored_error;
-			socket.write_some(asio::buffer(message), ignored_error);
-		}
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
+		cout << "Server start." << endl;
+		server srv;
+		srv.run();
+	} catch (std::exception &e) {
+		cout << e.what() << endl;
 	}
 
 	return 0;
