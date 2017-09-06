b2ee2db65b453f1b35de04b80ece6bcaa01a91bb
wangtao <niitwangtao@163.com>
2017-09-06 12:52:24 +0800

    update

--- SocketSer/server/server/server.cpp
+++ SocketSer/server/server/server.cpp
@@ -4,13 +4,52 @@
 #include "stdafx.h"
 #include "cstdio"
 #include "iostream"
+//#include "boost\asio.hpp"
+#include "boost\lexical_cast.hpp"
 
 using namespace std;
 
 int main()
 {
+	////≤‚ ‘ boost
+	//int i = 8848;
+	//char *mystr = "2013";
+	//i = boost::lexical_cast<int>(mystr);
+	//cout << "boost TypeTre:" << i << endl;
+
 	cout << "Server Hello world" << endl;
 	getchar();
 	return 0;
 }
+
+//#include "stdafx.h"
+//#include "ctime"
+//#include "iostream"
+//#include "string"
+//#include "boost\asio.hpp"
+//
+//using namespace boost;
+//using boost::asio::ip::tcp;
+//
+//int main() {
+//	try {
+//		asio::io_service io_service;
+//		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 3200));
+//
+//		for (;;) {
+//			tcp::socket socket(io_service);
+//			acceptor.accept(socket);
+//
+//			time_t now = time(0);
+//			std::string message = ctime(&now);
+//
+//			system::error_code ignored_error;
+//			socket.write_some(asio::buffer(message), ignored_error);
+//		}
+//	} catch (std::exception& e) {
+//		std::cerr << e.what() << std::endl;
+//	}
+//
+//	return 0;
+//}
 
