665e5296d0727aa430c898195f3c2a0e99a79136
wangtao <niitwangtao@163.com>
2017-09-06 10:39:42 +0800

    create pro

---
+++ SocketSer/server/server/server.cpp
@@ -0,0 +1,16 @@
+// server.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "cstdio"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	cout << "Server Hello world" << endl;
+	getchar();
+	return 0;
+}
+
